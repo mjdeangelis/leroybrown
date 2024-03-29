00cfb8e9dfdc9fab68a041c329fef0b6222d98ed
Mike DeAngelis <mikedeangelis@MikeMacbookPro.local>
2024-03-29 08:14:17 -0400

    Change button text

--- app/components/paymentForm.tsx
+++ app/components/paymentForm.tsx
@@ -113,7 +113,7 @@ export default function PaymentForm(props: PaymentFormProps) {
           onClick={submitPayment}
           className='rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
         >
-          Submit registration
+          Confirm payment
         </button>
       </div>
     </>
